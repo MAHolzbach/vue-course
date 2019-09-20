263b902ffe536aa7a9ac9a234f027922817c51bc
Mike Holzbach <mikeholzbach@carfax.com>
2019-09-20 10:14:37 -0400

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,19 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <button @click="selectedComponent = 'appQuote'">Quote</button>
-        <button @click="selectedComponent = 'appAuthor'">Author</button>
-        <button @click="selectedComponent = 'appNew'">New</button>
-        <hr>
-        <p>{{selectedComponent}}</p>
-        <keep-alive>
-          <component :is="selectedComponent"></component>
-        </keep-alive>
-        <!-- <app-quote>
-          <h2 slot="quoteTitle">{{quoteTitle}}</h2>
-          <p>Inspiring quote!</p>
-        </app-quote> -->
-      </div>
-    </div>
+
   </div>
 </template>
 
 <script>
-import Quote from "./components/Quote.vue";
-import Author from "./components/Author.vue";
-import New from "./components/New.vue";
-
 export default {
   data: function() {
     return {
-      quoteTitle: "The Quote:",
-      selectedComponent: "appQuote"
+      maxQuotes: 10,
+      quotes: ["Test quote."]
     };
-  },
-  components: {
-    appQuote: Quote,
-    appAuthor: Author,
-    appNew: New
   }
 };
 </script>
 
-<style scoped>
-h2 {
-  color: red;
-}
+<style>
 </style>
