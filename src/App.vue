36a08fb22db7807c24f020d55c9a47e6aab8f9bb
Mike Holzbach <mikeholzbach@carfax.com>
2019-09-20 09:37:49 -0400

    start

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,33 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <button @click="selectedComponent = 'appQuote'">Quote</button>
-        <button @click="selectedComponent = 'appAuthor'">Author</button>
-        <button @click="selectedComponent = 'appNew'">New</button>
-        <hr>
-        <p>{{selectedComponent}}</p>
-        <keep-alive>
-          <component :is="selectedComponent"></component>
-        </keep-alive>
-        <!-- <app-quote>
-          <h2 slot="quoteTitle">{{quoteTitle}}</h2>
-          <p>Inspiring quote!</p>
-        </app-quote> -->
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary">Load Blue Template</button>
+                <button class="btn btn-success">Load Green Template</button>
+                <button class="btn btn-danger">Load Red Template</button>
+                <hr>
+                <app-blue></app-blue>
+                <app-green></app-green>
+                <app-red></app-red>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import Quote from "./components/Quote.vue";
-import Author from "./components/Author.vue";
-import New from "./components/New.vue";
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
 
-export default {
-  data: function() {
-    return {
-      quoteTitle: "The Quote:",
-      selectedComponent: "appQuote"
-    };
-  },
-  components: {
-    appQuote: Quote,
-    appAuthor: Author,
-    appNew: New
-  }
-};
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
 </script>
 
-<style scoped>
-h2 {
-  color: red;
-}
+<style>
 </style>
