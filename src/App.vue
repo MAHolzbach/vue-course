80f99bee89ebd4ed8dab61710107acd77079ea4f
Mike Holzbach <mikeholzbach@carfax.com>
2019-10-04 14:10:34 -0400

    Project complete

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,48 @@
 <template>
   <div class="container">
-
+    <app-header
+      :quoteCount="quotes.length"
+      :maxQuotes="maxQuotes"
+    ></app-header>
+    <new-quote @quoteAdded="newQuote"></new-quote>
+    <app-quote-grid
+      :quotes="quotes"
+      @quoteDeleted='deleteQuote'
+    ></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-centered">
+        <div class="alert-info">Info: Click on a quote to delete it.</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+import NewQuote from "./components/NewQuote.vue";
+import Header from "./components/Header.vue";
 export default {
   data: function() {
     return {
       maxQuotes: 10,
       quotes: ["Test quote."]
     };
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete quotes first.");
+      }
+      this.quotes.push(quote);
+    },
+    deleteQuote(index) {
+      this.quotes.splice(index, 1);
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    newQuote: NewQuote,
+    appHeader: Header
   }
 };
 </script>
