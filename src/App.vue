05bd645cf331574c5bbcc06a83eec16411e2ab92
Mike Holzbach <mikeholzbach@carfax.com>
2019-11-09 12:02:13 -0500

    Exercise

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <div class="container">
     <form>
       <div class="row">
@@ -38,7 +38,6 @@
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
           <label for="message">Message</label><br>
-          <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
           <textarea
             id="message"
             rows="5"
@@ -184,4 +183,134 @@ export default {
 </script>
 
 <style>
+</style>-->
+
+<template>
+  <div class="container">
+    <form>
+      <div
+        class="row"
+        v-if="!isSubmitted"
+      >
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <!-- Exercise 1 -->
+          <!-- Create a Signup Form where you retrieve the following Information -->
+          <!-- Full Name (First Name + Last Name) -->
+          <!-- Mail -->
+          <!-- Password -->
+          <!-- Store Data? Yes/No -->
+          <div class="form-group">
+            <full-name v-model="fullName"></full-name>
+            <!-- <label for="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              class="form-control"
+              v-model="name"
+            > -->
+            <label for="name">Email</label>
+            <input
+              type="text"
+              id="email"
+              class="form-control"
+              v-model="email"
+            >
+            <label for="name">Password</label>
+            <input
+              type="text"
+              id="password"
+              class="form-control"
+              v-model="password"
+            >
+            <label for="">Store Data?
+              <label for="yes">Yes
+                <input
+                  type="radio"
+                  value="yes"
+                  id="storeDataYes"
+                  v-model="storeData"
+                >
+              </label>
+              <label for="no">No
+                <input
+                  type="radio"
+                  value="no"
+                  id="storeDataNo"
+                  v-model="storeData"
+                >
+              </label>
+            </label>
+          </div>
+          <!-- Exercise 2 -->
+          <!-- Only display the Form if it has NOT been submitted -->
+          <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+          <!-- Exercise 3 -->
+          <!-- Edit the Example from above and create a custom "Full Name" Control -->
+          <!-- which still holds the First Name and Last Name Input Field -->
+        </div>
+        <div class="row">
+          <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <button
+              class="btn btn-primary"
+              @click.prevent="submitted"
+            >Submit!
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+    <hr>
+    <div
+      class="row"
+      v-if="isSubmitted"
+    >
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Full Name: {{fullName}}</p>
+            <p>Mail: {{email}} </p>
+            <p>Password: {{password}} </p>
+            <p>Store in Database?: {{storeData}} </p>
+            <button
+              class="btn btn-warning"
+              @click.prevent="submitted"
+            >Reset
+            </button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FullName from "./FullName.vue";
+export default {
+  data() {
+    return {
+      fullName: "Mike Holzbach",
+      email: "",
+      password: "",
+      storeData: "no",
+      isSubmitted: false
+    };
+  },
+  methods: {
+    submitted() {
+      this.isSubmitted = !this.isSubmitted;
+    }
+  },
+  components: {
+    fullName: FullName
+  }
+};
+</script>
+
+<style>
 </style>
+
---
+++ src/FullName.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <label for="name">First Name</label>
+    <input
+      type="text"
+      id="firstName"
+      class="form-control"
+      :value="firstName"
+      @input="setName('first', $event)"
+    >
+    <label for="name">Last Name</label>
+    <input
+      type="text"
+      id="lastName"
+      class="form-control"
+      :value="lastName"
+      @input="setName('last', $event)"
+    >
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  methods: {
+    setName(inputName, event) {
+      let name = "";
+      inputName === "first"
+        ? (name = `${event.target.value} ${this.lastName}`)
+        : (name = `${this.firstName} ${event.target.value}`);
+      this.value = name;
+      this.$emit("input", this.value);
+    }
+  },
+  computed: {
+    firstName() {
+      return this.value.split(" ")[0];
+    },
+    lastName() {
+      return this.value.split(" ")[1];
+    }
+  }
+};
+</script>
+
+<style>
+</style>
